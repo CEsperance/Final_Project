f81da8440cff580bc693c057ce6fcb3876dd9c01
Angela Spencer <livesimplyspencer@gmail.com>
2022-03-05 09:36:56 -0600

    experimenting with vsc and updating pp wb

---
+++ Data_Preprocessing.py
@@ -0,0 +1,10 @@
+#import numpy as np
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import warnings
+
+#from sklearn.preprocessing import Normalizer
+
+hate_crime = pd.read_csv('hate_crime.csv')
+print(hate_crime.shape)
+print(hate_crime.head)
